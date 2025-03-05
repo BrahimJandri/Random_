d03024e24251020d19762561cf836991eae0b509
BrahimJandri <bzaza066@gmail.com>
2025-03-05 16:39:09 +0000

    work on understand the execption well

--- CPP/Exception/main.cpp
+++ CPP/Exception/main.cpp
@@ -5,8 +5,10 @@ int main()
 {
     try
     {
-        exception test("brahim", 29, 30000); 
-        std::cout << test << std::endl;   
+        exception test1;
+        exception test("brahim", 29, 30000);
+        test1 = test; 
+        std::cout << test1 << std::endl;   
     }
     catch(const std::exception& e)
     {
