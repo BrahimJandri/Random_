56a19841e7e0a5961bafc0cc252501e7398b4345
BrahimJandri <bzaza066@gmail.com>
2025-05-21 14:50:00 +0100

    merging sort

--- CPP/SortInMerge/SortInMerge.cpp
+++ CPP/SortInMerge/SortInMerge.cpp
@@ -1,83 +1,127 @@
 #include <iostream>
 #include <vector>
-#include <sstream>
-#include <cstdlib>
+#include <algorithm> // for std::sort
+#include <deque>
 
-std::vector<int> vecto;
+// Utility print function
 
-void FillArray(int ac, char **av)
+
+// Generate Jacobsthal sequence up to n elements
+std::deque<int> generateJacobsthal(int n)
 {
-    for (int i = 0; i < ac; i++)
+    std::deque<int> J(n);
+    if (n > 0)
+        J[0] = 0;
+    if (n > 1)
+        J[1] = 1;
+    for (int i = 2; i < n; ++i)
     {
-        std::stringstream ss(av[i]);
-        int number;
-        ss >> number;
-
-        if (ss.fail())
-        {
-            std::cerr << "Invalid number: " << av[i] << std::endl;
-            exit(1);
-        }
-        vecto.push_back(number);
+        J[i] = J[i - 1] + 2 * J[i - 2];
     }
+    return J;
 }
 
-// 🧠 Recursive Merge Sort Function
-void mergeInsertSort(std::vector<int> &container, int left, int right)
+// Insert small elements into big vector at positions based on Jacobsthal sequence
+void insertSmallIntoBig(std::deque<int> &big, const std::deque<int> &small)
 {
-    if (left >= right)
+    int n = small.size();
+    if (n == 0)
         return;
-    std::cout << "DKHLT" << std::endl;
-    int mid = (left + right) / 2;
-    mergeInsertSort(container, left, mid);
-    mergeInsertSort(container, mid + 1, right);
 
-    std::vector<int> temp;
-    int i = left, j = mid + 1;
+    // Generate Jacobsthal indices (starting from 1)
+    std::deque<int> jac = generateJacobsthal(n + 2);
 
-    while (i <= mid && j <= right)
+    // Insert small elements into big in Jacobsthal order
+    for (int i = 0; i < n; ++i)
     {
-        std::cout << "[i]: " << i << std::endl;
-        std::cout << "[j]: " << j << std::endl;
-        std::cout << "container[i]: " << container[i] << std::endl;
-        std::cout << "container[j]: " << container[j] << std::endl;
-        if (container[i] < container[j])
-            temp.push_back(container[i++]);
+        int pos = jac[i + 1]; // skip J(0) = 0, start from J(1)
+        if (pos > (int)big.size())
+            pos = big.size();
+
+        // Insert small[i] in sorted order in big (simulate insertion)
+        // Since big is sorted, we can do binary insert, but for simplicity linear insert:
+        std::deque<int>::iterator it = big.begin();
+        while (it != big.end() && *it < small[i])
+            ++it;
+        big.insert(it, small[i]);
+    }
+}
+
+// void mergeInsertSort(std::vector<int> &container)
+// {
+//     if (container.size() <= 1)
+//         return;
+
+//     std::vector<int> big;
+//     std::vector<int> small;
+
+//     // Pair up elements, split into big and small
+//     for (size_t i = 0; i + 1 < container.size(); i += 2)
+//     {
+//         if (container[i] > container[i + 1])
+//         {
+//             big.push_back(container[i]);
+//             small.push_back(container[i + 1]);
+//         }
+//         else
+//         {
+//             big.push_back(container[i + 1]);
+//             small.push_back(container[i]);
+//         }
+//     }
+
+//     // If odd number, last element goes to big (already sorted by default)
+//     if (container.size() % 2 == 1)
+//     {
+//         big.push_back(container.back());
+//     }
+
+//     // Sort big recursively or iteratively (here simple std::sort for demo)
+//     std::sort(big.begin(), big.end());
+
+//     // Insert small elements into big using Jacobsthal order insertion
+//     insertSmallIntoBig(big, small);
+
+//     container = big;
+
+//     print(big, small);
+// }
+
+void mergeInsertSort(std::deque<int> &container)
+{
+    if (container.size() <= 1)
+        return;
+
+    std::deque<int> big;
+    std::deque<int> small;
+
+    // Pair up elements, split into big and small
+    for (size_t i = 0; i + 1 < container.size(); i += 2)
+    {
+        if (container[i] > container[i + 1])
+        {
+            big.push_back(container[i]);
+            small.push_back(container[i + 1]);
+        }
         else
-            temp.push_back(container[j++]);
+        {
+            big.push_back(container[i + 1]);
+            small.push_back(container[i]);
+        }
     }
 
-    while (i <= mid)
-        temp.push_back(container[i++]);
-    while (j <= right)
-        temp.push_back(container[j++]);
-
-    for (int k = left; k <= right; ++k)
-        container[k] = temp[k - left];
-}
-
-void SortInMerge(std::vector<int> &vec)
-{
-    mergeInsertSort(vec, 0, vec.size() - 1);
-
-    // Print the sorted array
-    std::cout << "Sorted output: ";
-    for (size_t i = 0; i < vec.size(); ++i)
-        std::cout << vec[i] << " ";
-    std::cout << std::endl;
-}
-
-int main(int ac, char **av)
-{
-    if (ac == 1)
+    // If odd number, last element goes to big (already sorted by default)
+    if (container.size() % 2 == 1)
     {
-        std::cerr << "Error: Please enter some numbers!" << std::endl;
-        return 1;
+        big.push_back(container.back());
     }
 
-    FillArray(ac - 1, av + 1);
-    SortInMerge(vecto);
+    // Sort big recursively or iteratively (here simple std::sort for demo)
+    std::sort(big.begin(), big.end());
 
-    return 0;
+    // Insert small elements into big using Jacobsthal order insertion
+    insertSmallIntoBig(big, small);
+
+    container = big;
 }
 
